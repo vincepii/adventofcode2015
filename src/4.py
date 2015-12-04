d961b55d5ce61b349e74bda5a778570f19d767bf
Vincenzo Pii <vinc.pii@gmail.com>
2015-12-04 19:47:39 +0100

    Day 4

---
+++ src/4.py
@@ -0,0 +1,43 @@
+#! /usr/bin/env python
+
+import hashlib
+from multiprocessing import Process, Lock, Manager
+
+INPUT = 'iwrupvqb'
+STEP = 400000
+MAX_PROC = 4
+STARTHASH='000000'
+iteration_results = Manager().list()
+
+
+def checkhash(lock, start, end):
+    for i in range(start, end):
+        s = gethash(INPUT + str(i))
+        if s.startswith('000000'):
+            iteration_results.append(i)
+            break
+
+def gethash(key):
+    return hashlib.md5(key).hexdigest()
+
+if __name__ == '__main__':
+    rangestart = 0
+    l = Lock()
+    while True:
+        processes = []
+        for i in range(0, MAX_PROC):
+            p = Process(target=checkhash, args=(l, rangestart + STEP * i, rangestart + STEP * (i + 1)))
+            processes.append(p)
+            p.start()
+        # Synchronize with the processes
+        for p in processes:
+            p.join()
+        # Check if some process found a result
+        l.acquire()
+        if len(iteration_results) > 0:
+            print min(iteration_results)
+            l.release()
+            break
+        l.release()
+        # Prepare the next iteration
+        rangestart = rangestart + MAX_PROC * STEP
